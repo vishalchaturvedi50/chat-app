32b96fb41980b22465025365172bec4faf7915ce
Vishal Chaturvedi <vishalchaturvedi50@gmail.com>
2019-06-22 18:01:16 +0530

    friends componenet code comments

--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -10,30 +10,40 @@ import { UserService } from '../services/user.service';
 })
 export class FriendsComponent implements OnInit {
 
-
+  /* Friend list */
   public friendList: Array<UserClass> = [];
 
   constructor(public userService: UserService) { }
 
   ngOnInit() {
+    /* ON INIT SUBS. to user change and and initialize friend list accordingly */
     this.userService.changeInUserSubs.subscribe(resp => {
       if (resp == UserChangeEnum.CurrentUser) {
-        this.initializeFriendListFn();
+        this.getFriendListFn();
       }
     });
-    this.initializeFriendListFn();
+    this.getFriendListFn();
   }
 
-  initializeFriendListFn() {
+  /* Function to get Friend list */
+  getFriendListFn() {
     this.friendList = this.userService.getFriendListFn();
   }
 
+  /* WHEN a user is clicked from the list change current chat user */
   onUserItemClickFn(ev: any) {
-    let requiredListItem = ev.path.filter(x => x.localName == "li")[0];
-    let requiredUserId = requiredListItem.id.split("user")[1];
-    this.userService.setCurrentChatUserFn(parseInt(requiredUserId));
+    //GET THE required li item from the chain
+    let requiredListItemArr = ev.path.filter(x => x.localName == "li");
+    //if we get the item
+    if (requiredListItemArr.length > 0) {
+      //split and get user id
+      let requiredUserId = requiredListItemArr[0].id.split("user")[1];
+      //send it the user service for current user change
+      this.userService.setCurrentChatUserFn(parseInt(requiredUserId));
+    }
   }
 
+  /* Track function for ngFor */
   trackFriendListFn(index, item) {
     if (item)
       return item.id;
