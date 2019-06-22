16d3fac1f612db6a3aa68faa0b0c812d2958df45
Vishal Chaturvedi <vishalchaturvedi50@gmail.com>
2019-06-22 22:42:28 +0530

    style changes

--- src/app/models/constant.ts
+++ src/app/models/constant.ts
@@ -5,14 +5,13 @@ import { ChatMessage } from './message';
 export const userList: Array<UserClass> = [
     { id: 1, userName: "Vishal Chat", labels: "Drummer | JS Enthusiast" },
     { id: 2, userName: "John Anderson", labels: "Beer Brewing | Swimmer" },
-    { id: 3, userName: "JONES Brown ", labels: "Biker | F# | Python " },
+    { id: 3, userName: "Jones Brown ", labels: "Biker | F# | Python " },
     { id: 4, userName: "Larry Rock", labels: "Cooking | Baseball" },
     { id: 5, userName: "Olivia Larry", labels: "Motorcycling | Cooking" },
     { id: 6, userName: "Jessica Martinez", labels: "Cricket | Biker" },
     { id: 7, userName: "Daisy Raly", labels: "Golfing | Basketball" },
     { id: 8, userName: "Angelina Ray", labels: "Carpentry | Dancing" },
-    { id: 9, userName: "Ray Lopez", labels: "Investeing | Cricket" },
-    { id: 10, userName: "Daisy Raly", labels: "Golfing | Biker" }
+    { id: 9, userName: "Ray Lopez", labels: "Investeing | Cricket" }
 ];
 
 /* Function to get image path */
